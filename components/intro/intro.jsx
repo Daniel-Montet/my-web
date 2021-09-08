1cb0998f516db99d84b5ba348542d15eb838adfa
Daniel Montet <dmontetproff@gmail.com>
2021-09-08 23:06:42 +0300

    added fonts to intro section text

--- components/intro/intro.jsx
+++ components/intro/intro.jsx
@@ -2,14 +2,17 @@ import styles from "./intro.module.css";
 import HeroImage from "/public/hero-image.svg";
 
 export default function Intro() {
-  console.log(HeroImage)
+  console.log(HeroImage);
   return (
     <div className={styles.container}>
       <div className={styles.inner}>
         <div className={styles.textBox}>
-          <h3>Hello, my name is</h3>
-          <h2>Daniel Montet</h2>
-          <h3>A fullstack developer.</h3>
+          <nav className={styles.nav}></nav>
+          <div>
+            <h2 className={styles.h2}>Hello, my name is</h2>
+            <h1 className={styles.h1}>Daniel Montet</h1>
+            <h3 className={styles.h3}>A fullstack developer.</h3>
+          </div>
         </div>
         <div className={styles.svgBox}>
           <img className={styles.img} src={HeroImage.src} alt="" />
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&family=Rammetto+One&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
