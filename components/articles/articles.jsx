37a31e25ff8e453386261d77aaabf310c3ef7176
Daniel Montet <dmontetproff@gmail.com>
2021-09-14 08:19:36 +0300

    env file created, dynamic post page created

--- components/articles/articles.jsx
+++ components/articles/articles.jsx
@@ -1,34 +1,69 @@
 import styles from "./articles.module.css";
+import Link from "next/link";
 
-export default function Articles({ posts }) {
-  const NoPosts = () => (
+export default function Articles({ posts, meta }) {
+  let page = meta.pagination.page;
+  let next = meta.pagination.next;
+  let prev = meta.pagination.prev;
+
+  if (prev) {
+    prevButton = <span>prev</span>;
+  } else {
+    prevButton = <span></span>;
+  }
+  if (next) {
+    nextButton = <span>next</span>;
+  } else {
+    nextButton = <span></span>;
+  }
+
+  const NoPosts = (
     <div>
       <h2>Not Found</h2>
     </div>
   );
 
-  const Posts = () => (
-    <div className={styles.inner}>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
   if (posts === null) {
-    console.log("ran it");
-    return (
-      <div className="container">
-        <NoPosts />
-      </div>
-    );
+    // console.log("ran it");
+    return <div className="container">{NoPosts}</div>;
   }
-  console.log(posts);
+  // console.log(posts);
   return (
     <div className="container">
-      <Posts />
+      <div className={styles.inner}>
+        <div>
+          <h3>Articles</h3> <hr />
+        </div>
+        <Posts posts={posts} />
+        <div>
+          {prev}
+          <hr />
+          <span>Page: {page}</span>
+          <hr />
+          {next}
+        </div>
+      </div>
     </div>
   );
 }
+
+// renders post and a short excerpt
+const Posts = ({ posts }) => {
+  return (
+    <div>
+      {posts.map((post) => {
+        return (
+          <div key={post.id}>
+            <Link href="/posts/[slug]" as={`/posts/${post.slug}`}>
+              <a>{post.title}</a>
+            </Link>
+            <p>{post.custom_excerpt}</p>
+            <span>{post.published_at}</span>
+            <hr />
+            <span>{post.reading_time}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,40 @@ import Articles from "../components/articles/articles";
 import Intro from "../components/intro/intro";
 import Nav from "../components/navigation/nav";
 import Me from "../components/me/me";
-import { getPosts } from "../lib/posts";
+// import { getPosts } from "../lib/posts";
 
-export default function Home(posts) {
+let BLOG_URL = process.env.BLOG_URL;
+let CONTENT_API_KEY = process.env.CONTENT_API_KEY;
+
+const getPosts = async () => {
+  let data = await fetch(
+    `${BLOG_URL}/ghost/api/v3/content/posts?key=${CONTENT_API_KEY}&page=3&fields=title,custom_excerpt,published_at,reading_time,slug`
+  ).then((res) => res.json());
+
+  return data;
+};
+
+export default function Home({ meta, posts }) {
   return (
     <>
       <Nav />
       <Intro />
       <Me />
-      <Articles posts={posts.posts} />
+      <Articles posts={posts} meta={meta} />
     </>
   );
 }
 
 export async function getStaticProps(context) {
-  const posts = await getPosts();
+  const { posts, meta } = await getPosts();
 
   if (!posts) {
     return {
-      props: { posts: null },
+      props: { posts: null, meta: null },
     };
   }
 
   return {
-    props: { posts },
+    props: { posts, meta },
   };
 }
