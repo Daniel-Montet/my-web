e17cbc662238d4a342b02e97dcee4642ef10b304
Daniel Montet <dmontetproff@gmail.com>
2021-09-21 10:22:52 +0300

    these changes are irrelevant, ill override them on merge with origin

--- components/intro/intro.jsx
+++ components/intro/intro.jsx
@@ -3,6 +3,7 @@ import HeroImage from "/public/hero-image.svg";
 
 export default function Intro() {
   return (
+    <>
     <div className={styles.container}>
       <div className={styles.inner}>
         <div className={styles.textBox}>
@@ -18,5 +19,21 @@ export default function Intro() {
         </div>
       </div>
     </div>
+    <div className={styles.container}>
+    <div className={styles.inner}>
+      <div className={styles.textBox}>
+        <nav className={styles.nav}></nav>
+        <div>
+          <h2 className={styles.h2}>Hello, my name is</h2>
+          <h1 className={styles.h1}>Daniel Montet</h1>
+          <h3 className={styles.h3}>A fullstack developer.</h3>
+        </div>
+      </div>
+      <div className={styles.svgBox}>
+        <img className={styles.img} src={HeroImage.src} alt="" />
+      </div>
+    </div>
+  </div>
+  </>
   );
 }
