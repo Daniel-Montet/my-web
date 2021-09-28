2687abba3620334db81323503ab1bbf617f44492
Daniel Montet <dmontetproff@gmail.com>
2021-09-28 15:34:41 +0300

    commented out child component in the Layout component that breaks the build

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -2,10 +2,10 @@ export default function Layout(Nav,Content) {
     return (
         <section className="grid">
       <section className="item1">
-        <Nav />
+        {/* <Nav /> */}
       </section>
       <section className="item2">
-        <Articles posts={posts} tags={tags} />
+        {/* <Articles posts={posts} tags={tags} /> */}
       </section>
     </section>
     )
