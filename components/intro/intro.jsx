233bacfe477e0489af8bf441fe97e91ac08916be
Daniel Montet <dmontetproff@gmail.com>
2021-10-02 08:37:01 +0300

    bleh

--- components/intro/intro.jsx
+++ components/intro/intro.jsx
@@ -3,19 +3,15 @@ import HeroImage from "/public/hero-image.svg";
 
 export default function Intro() {
   return (
-    <div className="container">
-      <div className={styles.inner}>
-        <div className={styles.textBox}>
-          <div>
-            <h2 className={styles.h2}>Hello, my name is</h2>
-            <h1 className={styles.h1}>Daniel Montet</h1>
-            <h3 className={styles.h3}>A fullstack developer.</h3>
-          </div>
-        </div>
-        <div className={styles.svgBox}>
-          <img className={styles.img} src={HeroImage.src} alt="" />
-        </div>
-      </div>
-    </div>
+    <section className="intro inner-section">
+      <section className="col-1">
+        <h4 className={styles.h2}>Hello, my name is</h4>
+        <h1 className={styles.h1}>Daniel Montet</h1>
+        <h3 className={styles.h3}>A fullstack developer.</h3>
+      </section>
+      <section className="col-2">
+        <img src={HeroImage.src} alt="hero image" />
+      </section>
+    </section>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
 import Intro from "../components/intro/intro";
 import Me from "../components/me/me";
 import { Grid } from "../components/layout/layout";
-import Excerpt from "../components/articles/art/art";
+import Excerpt from "../components/articles/articles";
 
 let BLOG_URL = process.env.BLOG_URL;
 let CONTENT_API_KEY = process.env.CONTENT_API_KEY;
 
 export default function Home({ postsMeta, posts, tags, tagsMeta }) {
   return (
-      <Grid stacked fullHeight>
+    <Grid stacked fullHeight>
       <Intro />
-      <Me/>
-        <Excerpt
-          posts={posts}
-          postsMeta={postsMeta}
-          tags={tags}
-          tagsMeta={tagsMeta}
-        />
-      </Grid>
+      {/* <Me />
+      <Excerpt
+        posts={posts}
+        postsMeta={postsMeta}
+        tags={tags}
+        tagsMeta={tagsMeta}
+      /> */}
+    </Grid>
   );
 }
 
